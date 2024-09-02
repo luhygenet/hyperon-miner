ad6dbba9baef9dc19d06fd1153c6fdf66f7c1bb2
BaslaelWorkineh <baslaworku@gmail.com>
2024-09-02 12:34:31 +0300

    Implemented a function to find all combinations of two lists

--- experiments/rules/conj-exp/main.py
+++ experiments/rules/conj-exp/main.py
@@ -13,47 +13,125 @@ from hyperon.base import Tokenizer, SExprParser
 from hyperon.ext import register_atoms, register_tokens
 import hyperonpy as hp
 
-
-def remove_quotes_op(metta: MeTTa, *args):  
-
-    text = " ".join(str(atom) for atom in args)
-
-    unquoted_text = text.replace('"', '')
-
-    atoms = metta.parse_all(unquoted_text)
-    return [ValueAtom(atoms, 'Expression')]
-
-def map_variables(metta: MeTTa, *args):
-    cnj_var = args[0]
-    pat_var = args[1]
+def combine_lists_op(metta: MeTTa, var1, var2):
+    #($Y ($X ())) ($a  ($b ($c ())))
+    input_str1 = str(var1)
+    input_str2 = str(var2)
     
-    cnj_str = str(cnj_var)
-    pat_str = str(pat_var)
+    list1 = parse_list_structure(input_str1.replace('"', ''))
+    list2 = parse_list_structure(input_str2.replace('"', ''))
 
-    #TODO implement mapping logic here
-    mapped_pattern = "(It is working)"
+    combinations = combine_lists(list1, list2)
+    
+    unique_combos = unique_combinations(combinations, list1, list2)
+    
+    combined_pattern = " ".join(["({} {} {})".format(combo[0], combo[1], combo[2]) for combo in unique_combos])
+    combined_pattern_atoms = "(" + combined_pattern + ")"
 
-    atoms = metta.parse_all(mapped_pattern)
-    return [ValueAtom(atoms, 'Expression')]
+    atoms = metta.parse_all(combined_pattern_atoms)
+    return atoms
+
+def parse_list_structure(input_str):
+    """Convert a string with parentheses into a nested list structure."""
+    elements = []
+    current = ""
+    in_word = False
+    
+    for char in input_str:
+        if char == '(':
+            if in_word:
+                elements.append(f'"{current.strip()}", ')
+                current = ""
+                in_word = False
+            elements.append('[')
+        elif char == ')':
+            if in_word:
+                elements.append(f'"{current.strip()}"')
+                current = ""
+                in_word = False
+            elements.append('], ')
+        elif char.isspace():
+            if in_word:
+                elements.append(f'"{current.strip()}", ')
+                current = ""
+                in_word = False
+        else:
+            current += char
+            in_word = True
+    
+    if in_word:
+        elements.append(f'"{current.strip()}"')
+    
+    parsed_str = ''.join(elements)
+    
+    parsed_str = parsed_str.replace(', ]', ']')
+    parsed_str = parsed_str.rstrip(', ')
+    
+    return eval(parsed_str)
+
+def flatten_list(nested_list):
+    """Flatten a nested list structure into a flat list of elements."""
+    if isinstance(nested_list, list):
+        flat_list = []
+        for item in nested_list:
+            flat_list.extend(flatten_list(item))
+        return flat_list
+    else:
+        return [nested_list]
+
+def combine_lists_recursive(list1, list2, length, current_combination=None, index1=0, index2=0):
+    if current_combination is None:
+        current_combination = []
+    
+    if len(current_combination) == length:
+        return [current_combination]
+    
+    combinations = []
+    
+    for i in range(index1, len(list1)):
+        new_combination = current_combination + [list1[i]]
+        combinations.extend(combine_lists_recursive(list1, list2, length, new_combination, i + 1, index2))
+
+    for j in range(index2, len(list2)):
+        new_combination = current_combination + [list2[j]]
+        combinations.extend(combine_lists_recursive(list1, list2, length, new_combination, index1, j + 1))
+
+    return combinations
+
+def combine_lists(list1, list2):
+    flat_list1 = flatten_list(list1)
+    flat_list2 = flatten_list(list2)
+    length = max(len(flat_list1), len(flat_list2))
+    return combine_lists_recursive(flat_list1, flat_list2, length)
+
+def unique_combinations(combinations, list1, list2):
+    flat_list1 = flatten_list(list1)
+    flat_list2 = flatten_list(list2)
+    
+    seen = set()
+    unique_combos = []
+    list1_set = set(str(item) for item in flat_list1)
+    list2_set = set(str(item) for item in flat_list2)
+
+    for combo in combinations:
+        sorted_combo = tuple(sorted(str(item) for item in combo))
+        combo_set = set(sorted_combo)
+        if sorted_combo not in seen and combo_set != list1_set and combo_set != list2_set:
+            seen.add(sorted_combo)
+            unique_combos.append(combo)
+    return unique_combos
 
 @register_atoms(pass_metta=True)
 def cnj_exp(metta):
-    removeQuote = OperationAtom(
-        'rm_q', 
-        lambda *args: remove_quotes_op(metta, *args), 
-        [AtomType.ATOM, "Expression"], 
+    
+    combineLists = OperationAtom(
+        'combine_lists', 
+        lambda var1, var2: combine_lists_op(metta, var1, var2), 
+        ['Atom', 'Atom', 'Expression'], 
         unwrap=False)
-    mapVariables = OperationAtom(
-        'mp_var',
-        lambda *args: map_variables(metta, *args),
-        [AtomType.ATOM, AtomType.ATOM, "Expression"],
-        unwrap=False
-    )
-
-
+    
     return {
-        r"rm_q": removeQuote,
-        r"mp_var": mapVariables
+        r"combine_lists": combineLists
     }
 
 
