470dcad15e4a8868967a13fc5cf57cb6f4e59253
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-11-22 09:53:35 +0300

    changing representations and adding the link in front of the nodes fix

--- experiments/rules/freq-pat/main.py
+++ experiments/rules/freq-pat/main.py
@@ -7,14 +7,27 @@ import sys
 import os
 import time
 
-from hyperon.atoms import ExpressionAtom, E, GroundedAtom, OperationAtom, ValueAtom, NoReduceError, AtomType, MatchableObject, VariableAtom,\
-    G, S,V, Atoms, get_string_value, GroundedObject, SymbolAtom
+from hyperon.atoms import (
+    ExpressionAtom,
+    E,
+    GroundedAtom,
+    OperationAtom,
+    ValueAtom,
+    NoReduceError,
+    AtomType,
+    MatchableObject,
+    VariableAtom,
+    G,
+    S,
+    V,
+    Atoms,
+    get_string_value,
+    GroundedObject,
+    SymbolAtom,
+)
 from hyperon.base import Tokenizer, SExprParser
 from hyperon.ext import register_atoms, register_tokens
 import hyperonpy as hp
-
-
-
 
 
 # def call_python_process(metta: MeTTa, pattern):
@@ -37,7 +50,7 @@ def call_python_process(metta: MeTTa):
     #                     ($link $g $o)
     #                     ($link $nx $ny)
     #                 )
-    #         ) 
+    #         )
     # )
 
     # )
@@ -45,36 +58,43 @@ def call_python_process(metta: MeTTa):
     # ''')
 
     # run_str = f'!(abstract-recursive {pattern})'
-    run_str = f'!(match &specspace (SpecializationOf $c $d) (SpecializationOf $c $d))'
+    run_str = (
+        f"!(match &specredspace (SpecializationOf $c $d) (SpecializationOf $c $d))"
+    )
 
     patterns = metta.run(run_str)
 
     data = [str(item) for sublist in patterns for item in sublist]
 
     def clean_reserved_vars(expression):
-        expression = re.sub(r'\$x#\d+', '$x', expression)
-        expression = re.sub(r'\$y#\d+', '$y', expression)
-        expression = re.sub(r'\$link#\d+', '$link', expression)
+        expression = re.sub(r"\$x#\d+", "$x", expression)
+        expression = re.sub(r"\$y#\d+", "$y", expression)
+        expression = re.sub(r"\$link#\d+", "$link", expression)
         return expression
 
     # Extract structure while ignoring specific variables
     def extract_structure(expression):
         # Replace all other variables except $x, $y, and $link with a placeholder
         expression = clean_reserved_vars(expression)
-        return re.sub(r'\$\w+#\d+', '$var', expression)
+        return re.sub(r"\$\w+#\d+", "$var", expression)
+
     structure_dict = {}
     for expr in data:
         structure = extract_structure(expr)
         if structure not in structure_dict:
-            structure_dict[structure] = expr  
+            structure_dict[structure] = expr
 
     def generate_random_var():
-        return '$' + ''.join(random.choices(string.ascii_lowercase, k=1)) + ''.join(random.choices(string.digits, k=6))
+        return (
+            "$"
+            + "".join(random.choices(string.ascii_lowercase, k=1))
+            + "".join(random.choices(string.digits, k=6))
+        )
 
     unique_patterns = []
     for structure in structure_dict.keys():
         unique_structure = structure
-        var_count = len(re.findall(r'\$var', structure))
+        var_count = len(re.findall(r"\$var", structure))
 
         for _ in range(var_count):
             unique_var = generate_random_var()
@@ -83,16 +103,19 @@ def call_python_process(metta: MeTTa):
         # unique_patterns.append(unique_structure)
         # unique_patterns.append(ValueAtom(metta.parse_single(unique_structure), 'Expression'))
         unique_patterns.append(metta.parse_single(unique_structure))
-    # uni_patterns = ' '.join(unique_patterns)    
-    # atoms = metta.parse_single(uni_patterns)    
+    # uni_patterns = ' '.join(unique_patterns)
+    # atoms = metta.parse_single(uni_patterns)
     # return [ValueAtom(atoms, 'Expression')]
-        
+
     return unique_patterns
+
 
 @register_atoms(pass_metta=True)
 def redundancy(metta):
     # redundancyFreeAtom = OperationAtom('redunpat', lambda patterns: call_python_process(metta, patterns), unwrap=False)
-    redundancyFreeAtom = OperationAtom('redunpat', lambda: call_python_process(metta),['Expression'], unwrap=False)
+    redundancyFreeAtom = OperationAtom(
+        "redunpat", lambda: call_python_process(metta), ["Expression"], unwrap=False
+    )
     return {
         r"redunpat": redundancyFreeAtom,
     }
