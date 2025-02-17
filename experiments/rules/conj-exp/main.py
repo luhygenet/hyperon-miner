20bed1767bad0dd7c5eea3b5fcf86db2fb7dddfc
= <=>
2025-02-17 14:34:26 +0300

    permutation and combination and new unique combination rules

--- experiments/rules/conj-exp/main.py
+++ experiments/rules/conj-exp/main.py
@@ -8,14 +8,17 @@ from hyperon.ext import register_atoms
 import itertools
 from itertools import combinations
 
+
 def combine_lists_op(metta: MeTTa, var1, var2):
     input_str1 = str(var1)
     input_str2 = str(var2)
 
     list1 = parse_metta_structure(input_str1)
-    list2 = parse_metta_structure(input_str2) 
+    list2 = parse_metta_structure(input_str2)
+
 
     combinations = range_combinations(list1, list2)
+    # combinations = combine_lists(list1, list2)
     combined_pattern = " ".join(
         ["({})".format(" ".join(combo)) for combo in combinations]
     )
@@ -24,6 +27,7 @@ def combine_lists_op(metta: MeTTa, var1, var2):
     atoms = metta.parse_all(combined_pattern_atoms)
     return atoms
 
+
 def range_combinations(list1, list2):
     merged_list = list1 + list2
     i, j = len(list2), len(list2)
@@ -31,25 +35,27 @@ def range_combinations(list1, list2):
     for sub in range(j):
         if sub >= (i - 1):
             res.extend(combinations(merged_list, sub + 1))
-    
+
     res = [combo for combo in res if not set(combo).issubset(set(list2))]
 
     all_permutations = []
     for combo in res:
         perms = [list(p) for p in itertools.permutations(combo)]
         all_permutations.extend(perms)
-    return all_permutations
+    return unique_combinations(all_permutations, list1, list2)
+    # return all_permutations
+
 
 def parse_metta_structure(input_str):
     """Convert a string like ($A $B $C) into a flat list ['$A', '$B', '$C']"""
     elements = []
     current = ""
     in_word = False
-    
+
     for char in input_str:
-        if char == '(':
+        if char == "(":
             continue
-        elif char == ')':
+        elif char == ")":
             if in_word:
                 elements.append(current.strip())
                 current = ""
@@ -65,8 +71,9 @@ def parse_metta_structure(input_str):
 
     if in_word:
         elements.append(current.strip())
-    
+
     return elements
+
 
 def flatten_list(nested_list):
     flat_list = []
@@ -79,27 +86,39 @@ def flatten_list(nested_list):
             flat_list.append(current)
     return flat_list
 
+
 # Permutations not implemented yet and also the conjunction vars must be returned with the
 # generated combinations
-def combine_lists_recursive(list1, list2, length, current_combination=None, index1=0, index2=0):
+def combine_lists_recursive(
+    list1, list2, length, current_combination=None, index1=0, index2=0
+):
     if current_combination is None:
         current_combination = []
-    
+
     if len(current_combination) == length:
         return [current_combination]
-    
+
     combinations = []
-    
+
     for i in range(index1, len(list1)):
         new_combination = current_combination + [list1[i]]
-        combinations.extend(combine_lists_recursive(list1, list2, length, new_combination, i + 1, index2))
+        combinations.extend(
+            combine_lists_recursive(
+                list1, list2, length, new_combination, i + 1, index2
+            )
+        )
 
     for j in range(index2, len(list2)):
         new_combination = current_combination + [list2[j]]
-        combinations.extend(combine_lists_recursive(list1, list2, length, new_combination, index1, j + 1))
+        combinations.extend(
+            combine_lists_recursive(
+                list1, list2, length, new_combination, index1, j + 1
+            )
+        )
 
-    return (unique_combinations(combinations, list1, list2))
+    return unique_combinations(combinations, list1, list2)
     # return combinations
+
 
 def combine_lists(list1, list2):
     length = len(list2)
@@ -107,13 +126,12 @@ def combine_lists(list1, list2):
 
 
 def unique_combinations(combinations, list1, list2):
-    # print(list1)
     # print(list2)
     flat_list1 = flatten_list(list1)
     flat_list2 = flatten_list(list2)
     # print(flat_list1)
     # print(flat_list2)
-    
+
     seen = set()
     unique_combos = []
     list1_set = set(str(item) for item in flat_list1)
@@ -124,31 +142,50 @@ def unique_combinations(combinations, list1, list2):
         # sorted_combo = tuple(sorted(str(item) for item in combo))
         sorted_combo = tuple(str(item) for item in combo)
         combo_set = set(sorted_combo)
-        if sorted_combo not in seen and combo_set != list1_set and combo_set != list2_set:
-        # if combo_set != list1_set and combo_set != list2_set:
-            seen.add(sorted_combo)
-            unique_combos.append(combo)
+        if (
+            sorted_combo not in seen
+            and combo_set != list1_set
+            and combo_set != list2_set
+        ):
+            has_element_from_list1 = any(str(item) in list1_set for item in combo)
+            has_element_from_list2 = any(str(item) in list2_set for item in combo)
+
+            # If the combination has only one element, ensure it comes from list1
+            if len(combo) == 1:
+                if has_element_from_list1:
+                    seen.add(sorted_combo)
+                    unique_combos.append(combo)
+            # Otherwise, ensure it has at least one element from both lists
+            elif has_element_from_list1 and has_element_from_list2:
+                seen.add(sorted_combo)
+                unique_combos.append(combo)
     return unique_combos
     # return seen
+
+
 def generate_random_string(length=1):
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+    return "".join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
 
 def generate_random_var():
-    base_name = 'R-' + generate_random_string() + str(int(time.time()))
+    base_name = "R-" + generate_random_string() + str(int(time.time()))
     new_var = V(base_name)
 
     return [new_var]
 
+
 @register_atoms(pass_metta=True)
 def cnj_exp(metta):
-    combineLists = OperationAtom('combine_lists', lambda var1, var2: combine_lists_op(metta, var1, var2), 
-                                 ['Atom', 'Atom', 'Expression'],unwrap=False)
-    generateRandomVar = OperationAtom('generateRandomVar', lambda: generate_random_var(),
-                                      ['Expression'], unwrap=False)
-    return {
-        r"combine_lists": combineLists,
-        r"generateRandomVar": generateRandomVar
-    }
+    combineLists = OperationAtom(
+        "combine_lists",
+        lambda var1, var2: combine_lists_op(metta, var1, var2),
+        ["Atom", "Atom", "Expression"],
+        unwrap=False,
+    )
+    generateRandomVar = OperationAtom(
+        "generateRandomVar", lambda: generate_random_var(), ["Expression"], unwrap=False
+    )
+    return {r"combine_lists": combineLists, r"generateRandomVar": generateRandomVar}
 
 
-# print(combine_lists_op(MeTTa(), "($X $Y $X $B)", "($R-D1737102179 $A)"))
+print(combine_lists_op(MeTTa(), "($X $Y $X $B)", "($R-D1737102179 $A)"))
